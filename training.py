be07977699fab9ce92c3f4a6470639a085d9dd71
zhutao <zhutao@AY1304191107147256b5.(none)>
2013-06-02 17:44:32 +0800

    zhutao

--- training.py
+++ training.py
@@ -56,35 +56,45 @@ class Mysql:
     #select all value from calibration.
     def query_calibration_value(self):
         q = self.session.query(distinct(CalibrationOriginal.calibration_value))
+
         value_list = []
-        for v in q: value_list.append(v[0])
+        for v in q:
+            value_list.append(v[0])
+
         return value_list
 
         
     def query_calibration_by_value(self, value):
         q = self.session.query(distinct(CalibrationOriginal.calibration_timestamp_id)).filter(CalibrationOriginal.calibration_value == value)
+
         timestamp_list = []
-        for t in q: timestamp_list.append(t[0])
+        for t in q:
+            timestamp_list.append(t[0])
+
         return timestamp_list
 
     def query_calibration_by_value_timestamp(self, value, timestamp):
-        q = self.session.query(CalibrationOriginal.bssid, CalibrationOriginal.signal_strength).filter(CalibrationOriginal.calibration_value == value, CalibrationOriginal.calibration_timestamp_id == timestamp, CalibrationOriginal.signal_strength <= -50, CalibrationOriginal.signal_strength >= -90)
+        q = self.session.query(CalibrationOriginal.bssid, CalibrationOriginal.signal_strength).filter(CalibrationOriginal.calibration_value == value, CalibrationOriginal.calibration_timestamp_id == timestamp, CalibrationOriginal.signal_strength <= -35, CalibrationOriginal.signal_strength >= -95)
+
         ap_list = []
         for ap in q:
             #ap_list.append([ap[0], ap[1]])
             ap_list.append(ap[0])
+
         return ap_list
 
     def query_calibration_by_value_bssid(self, value, bssid):
         q = self.session.query(CalibrationOriginal.signal_strength).filter(CalibrationOriginal.calibration_value == value, CalibrationOriginal.bssid == bssid)
+
         ap_value_list = []
         for ap in q:
             ap_value_list.append(ap[0])
+
         return ap_value_list
         
 class CalibrationTimestamp(BaseModel):
     __tablename__ = 'calibration_timestamp'
-   
+    
     id = Column('id', Integer, primary_key=True)
     timestamp = Column('timestamp', Numeric(30, 10), primary_key=True)
 
@@ -130,36 +140,36 @@ if __name__ == "__main__":
     training_db = Mysql()
     training_db.open_session()
 
-    training_data = {}
     value_list = training_db.query_calibration_value()
+
     for v in value_list:
-        print "V : %d" % v
+        print "Value : %d" % v
         timestamp_list = training_db.query_calibration_by_value(v)
-        all_ap_list = []
-        ap_no = 0
+        ap_no = len(timestamp_list)
+
+        print "This value timesstamp Number : %d" % ap_no
+
+        all_times_ap_list = []
         for t in timestamp_list:
-            #print "T : %d" % t
-            ap_no = ap_no + 1
-            onetime_ap_list = training_db.query_calibration_by_value_timestamp(v, t)
-            all_ap_list.extend(onetime_ap_list)
-        #print "All times get AP : %s" % all_ap_list
-        #print "N : %d" % ap_no
+            one_time_ap_list = training_db.query_calibration_by_value_timestamp(v, t)
+            all_times_ap_list.extend(one_time_ap_list)
+            
         training_ap_list = []
-        for ap in all_ap_list:
-            if all_ap_list.count(ap) >= ap_no * 0.7:
+        for ap in all_times_ap_list:
+            if all_times_ap_list.count(ap) >= ap_no * 0.7:
                 if training_ap_list.count(ap) == 0:
                     training_ap_list.append(ap)
-        #print "Training AP list : %s" % training_ap_list
+
+        print "Training AP Number : %d" % len(training_ap_list)
+
         for ap in training_ap_list:
             one_ap_value_list = training_db.query_calibration_by_value_bssid(v, ap)
-            #print "AP bssid : %s" % ap
-            #print "Training ap value list : %s" % one_ap_value_list
+            
             ap_training_sum_value = 0.0
-            n = 0
             for i in one_ap_value_list:
                 ap_training_sum_value = ap_training_sum_value + (0.1 ** i)
-                n = n + 1
-            ap_training_value = ap_training_sum_value / n
+
+            ap_training_value = ap_training_sum_value / len(one_ap_value_list)
             print "AP bssid : %s => %f" % (ap, ap_training_value)
 
 
