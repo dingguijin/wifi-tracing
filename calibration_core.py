4a4f5d90dcfa742fa02b933568be77966223210e
Zhu Tao <zhutaoivy@gmail.com>
2013-06-09 21:18:00 +0800

    OK

--- calibration_core.py
+++ calibration_core.py
@@ -1,10 +1,17 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column
-from sqlalchemy.types import CHAR, Integer, String, DateTime, Numeric, Float
+from sqlalchemy.types import *
 from sqlalchemy.schema import ForeignKey
 from sqlalchemy import distinct
+
+import numpy as np
+from scipy import stats
+from sklearn import svm
 
 import datetime
 import time
@@ -36,7 +43,6 @@ class Mysql:
 
     def insert_calibration(self, d):
         assert(self.session)
-        #ts = datetime.datetime.fromtimestamp(d["timestamp"])
         ts = d["timestamp"]
         q = self.session.query(CalibrationTimestamp)
         qo = q.filter(CalibrationTimestamp.timestamp == ts).scalar()
@@ -52,29 +58,36 @@ class Mysql:
                                  d["calibration_value"], ts_id)
         self.session.add(co)
         self.session.commit()
-        
-    def query_calibration_by_value(self, value):
-        q = self.session.query(distinct(CalibrationOriginal.calibration_timestamp_id)).filter(CalibrationOriginal.calibration_value == value)
-        print q.count()
-        print type(q)
-        timestamp_list = []
-        for x in q:
-            print x
-            timestamp_list.append(x[0])
-        print timestamp_list
-        return q
-        
-        
-class CalibrationTimestamp(BaseModel):
-    __tablename__ = 'calibration_timestamp'
-   
-    id = Column('id', Integer, primary_key=True)
-    timestamp = Column('timestamp', Numeric(30, 10), primary_key=True)
 
-    def __init__(self, ts):
-        self.id = None
-        self.timestamp = ts
+    def query_calibration(self, sql):
+        q = self.session.execute(sql)
 
+        ret = []
+        for line in q:
+            ret.append(line[0])
+
+        return ret
+
+    def query_calibration2(self, sql):
+        q = self.session.execute(sql)
+
+        ret = []
+        for line in q:
+            ret.append([line[0], line[1]])
+
+        return ret
+
+    def query_calibration3(self, sql):
+        q = self.session.execute(sql)
+
+        ret = []
+        for line in q:
+            ret.append([line[0], line[1], line[2]])
+
+        return ret
+        
+#------------------------------------------------------------------------------
+        
 class CalibrationOriginal(BaseModel):
     __tablename__ = 'calibration_original'
 
@@ -106,32 +119,75 @@ class CalibrationOriginal(BaseModel):
         d["calibration_timestamp_id"] = self.calibration_timestamp_id
         return str(d)
 
-def test_insert_calibration(test_db):
-    test_db.open_session()
-    for i in range(1000):
-        tu = datetime.datetime.now().timetuple()
-        d = {}
-        #d["timestamp"] = time.mktime(tu)
-        d["timestamp"] = time.time()
-        d["ssid"] = "ssid"
-        d["bssid"] = "bssid"
-        d["signal_strength"] = -100
-        d["calibration_value"] = -1
-        test_db.insert_calibration(d)
-    test_db.close_session()
+########################################################################
 
-def test_constuct():
+def calibration_training():
+    #得到所有采样区域
+    value = src_db.query_calibration("select distinct calibration_value \
+                                      from calibration_original")
+    for v in value:
+        #得到一个区域采集的时间点
+        time_id = src_db.query_calibration("select distinct calibration_timestamp_id \
+                                            from calibration_original \
+                                            where calibration_value = %d" % v)
+        #得到一个区域一次采集到的AP物理地址
+        ap_all_list = []
+        for t in time_id:
+            bssid = src_db.query_calibration("select bssid \
+                                              from calibration_original \
+                                              where calibration_value = %d and calibration_timestamp_id = %d" % (v, t))
+            ap_all_list.extend(bssid)
+
+        #从一个区域一次手机到的AP中选出出现频率大于90%的AP
+        ap_training_list = []
+        for ap in ap_all_list:
+            if ap_all_list.count(ap) >= len(time_id) * 0.9:
+                if ap_training_list.count(ap) == 0:
+                    ap_training_list.append(ap)
+
+        signal_mean_list = []
+        sigma_list = []
+        for ap in ap_training_list:
+            #根据区域和选出的有效AP 得到该AP的所有信号指数
+            s = src_db.query_calibration("select signal_strength \
+                                         from calibration_original \
+                                         where calibration_value = %d and bssid = \'%s\'" % (v, ap))
+
+            signal_src_array = np.array(s, dtype = np.float64)
+            signal_array = np.array(s, dtype = np.float64)
+
+            #将信号指数计算成物理信号值
+            i = 0
+            for s_t in signal_array:
+                signal_array[i] = 10 ** (s_t / 10)
+                i = i + 1
+                
+            #计算出一个AP的平均物理信号值 再还原为该AP的平均信号指数
+            signal_mean = 10 * np.log10(signal_array.mean())
+
+            #根据AP的平均信号指数计算出AP的信号方差
+            sigma = np.float64(0.0)
+            for j in s:
+                sigma = sigma + np.float64((j - signal_mean) ** 2.0)
+
+            #将AP的信号平均指数和方差保存为列表
+            signal_mean_list.append(signal_mean)
+            sigma_list.append(sigma / np.float64(len(signal_src_array) - 1))
+
+        print "value :", v, "ap_training_list len :", len(ap_training_list)
+        print "signal mean:", signal_mean_list
+        print "sigma :", sigma_list
+
+        #将校准数据存入数据库
+        
+
+def calibration_core():
     pass
 
-def test_calibration(db, value):
-    db.open_session()
-    q = db.query_calibration_by_value(value)
-    db.close_session()
-
 if __name__ == "__main__":
-    test_db = Mysql()
-    #test_db.drop_db()
-    #test_db.init_db()
-    #test_insert_calibration(test_db)
-    test_calibration(test_db, 2)
+    src_db = Mysql()
+    src_db.open_session()
+    calibration_training()
+    calibration_core()
+    src_db.close_session()
 
